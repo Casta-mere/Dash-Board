819cf3dbbeeab0eb9a9fbf19776e81274378cdf1
Casta-mere <castamerego@gmail.com>
2024-02-21 18:22:30 +0800

    static & dynamic rendering

--- dash-board/app/users/page.tsx
+++ dash-board/app/users/page.tsx
@@ -8,12 +8,12 @@ interface User {
 const UserPage = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users", {
     cache: "no-store",
-    next: { revalidate: 10 },
   });
   const users: User[] = await res.json();
   return (
     <>
       <h1>User</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
