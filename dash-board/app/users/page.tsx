0fbb14be7b0ed0b37e2cbcc2c398558d3361c417
Casta-mere <castamerego@gmail.com>
2024-02-21 18:00:04 +0800

    caching problem

--- dash-board/app/users/page.tsx
+++ dash-board/app/users/page.tsx
@@ -1,7 +1,26 @@
 import React from "react";
 
-const UserPage = () => {
-  return <div>UserPage</div>;
+interface User {
+  id: number;
+  name: string;
+}
+
+const UserPage = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users", {
+    cache: "no-store",
+    next: { revalidate: 10 },
+  });
+  const users: User[] = await res.json();
+  return (
+    <>
+      <h1>User</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default UserPage;
