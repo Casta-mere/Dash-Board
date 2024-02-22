5827282ab172fff9e59e370433f0788c13d77cf9
Casta-mere <castamerego@gmail.com>
2024-02-22 11:19:55 +0800

    css modules

--- dash-board/app/components/ProductCard/ProductCard.tsx
+++ dash-board/app/components/ProductCard/ProductCard.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import AddToCart from "./AddToCart";
+import AddToCart from "../AddToCart";
+import style from "./ProducCard.module.css";
 
 const ProductCard = () => {
   return (
-    <div>
+    <div className={style.card}>
       <AddToCart />
     </div>
   );
