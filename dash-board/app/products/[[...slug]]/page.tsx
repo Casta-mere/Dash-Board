522b0e9422fe574e975d441f60b837251726b71d
Casta-mere <castamerego@gmail.com>
2024-02-23 15:50:42 +0800

    Access Parameters

--- dash-board/app/products/[[...slug]]/page.tsx
+++ dash-board/app/products/[[...slug]]/page.tsx
@@ -2,10 +2,19 @@ import React from "react";
 
 interface Props {
   params: { slug: string[] };
+  searchParams: { sortOrder: string };
 }
 
-const ProductPage = ({ params: { slug } }: Props) => {
-  return <div>ProductPage {slug && slug.map((str) => str + " ")}</div>;
+const ProductPage = ({
+  params: { slug },
+  searchParams: { sortOrder },
+}: Props) => {
+  return (
+    <>
+      ProductPage {slug && slug.map((str) => str + " ")}{" "}
+      {sortOrder && "sortOrder = " + sortOrder}
+    </>
+  );
 };
 
 export default ProductPage;
