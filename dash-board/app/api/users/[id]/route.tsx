7225a6b9d189ba9463dd5df72c517a7881f94489
Casta-mere <castamerego@gmail.com>
2024-02-24 10:33:35 +0800

    Updating Objects

--- dash-board/app/api/users/[id]/route.tsx
+++ dash-board/app/api/users/[id]/route.tsx
@@ -8,3 +8,23 @@ export function GET(
     return NextResponse.json({ error: "User not found" }, { status: 404 });
   return NextResponse.json({ id, name: "Castamere" });
 }
+
+// Use put for replacing
+// Use patch for updating some properties
+export async function PUT(
+  request: NextRequest,
+  { params: { id } }: { params: { id: number } }
+) {
+  // Validate the request body
+  const body = await request.json();
+  // If invalid, return 400
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 });
+  // Fetch the user
+  // If does not exist, return 404
+  if (id > 10)
+    return NextResponse.json({ error: "User not found" }, { status: 404 });
+  // Update the user
+  // Return the updated user
+  return NextResponse.json({ id, name: body.name });
+}
