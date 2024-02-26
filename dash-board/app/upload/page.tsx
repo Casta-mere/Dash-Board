043b5ee6be4ada0fa5d4c82de14cc3c9c1e771d0
Casta-mere <castamerego@gmail.com>
2024-02-26 12:43:25 +0800

    Show Image

--- dash-board/app/upload/page.tsx
+++ dash-board/app/upload/page.tsx
@@ -1,16 +1,33 @@
 "use client";
-import React from "react";
-import { CldUploadWidget } from "next-cloudinary";
+import React, { useState } from "react";
+import { CldUploadWidget, CldImage } from "next-cloudinary";
+
+interface CloudinrayResult {
+  public_id: string;
+}
 
 const UploadPage = () => {
+  const [publicId, setPublicId] = useState("");
   return (
-    <CldUploadWidget uploadPreset="bf9xjxfb">
-      {({ open }) => (
-        <button className="btn btn-secondary" onClick={() => open()}>
-          Upload
-        </button>
+    <>
+      {publicId && (
+        <CldImage src={publicId} width={270} height={180} alt={"a picture"} />
       )}
-    </CldUploadWidget>
+      <CldUploadWidget
+        uploadPreset="bf9xjxfb"
+        onUpload={(result, widget) => {
+          if (result.event !== "success") return;
+          const info = result.info as CloudinrayResult;
+          setPublicId(info.public_id);
+        }}
+      >
+        {({ open }) => (
+          <button className="btn btn-secondary" onClick={() => open()}>
+            Upload
+          </button>
+        )}
+      </CldUploadWidget>
+    </>
   );
 };
 export default UploadPage;
