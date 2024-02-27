5e351cce6ffa2351cc3d5eb497a38e493c26f9e2
Casta-mere <castamerego@gmail.com>
2024-02-27 18:56:04 +0800

    Protecting Routes

--- dash-board/app/api/auth/[...nextauth]/route.ts
+++ dash-board/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,13 @@
 import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
-// import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
     Github({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
