b724ecdbf8a6edd30b6bc2ff72e8bf9145e73a66
Casta-mere <castamerego@gmail.com>
2024-02-27 18:16:46 +0800

    Accessing Session from server

--- dash-board/app/api/auth/[...nextauth]/route.ts
+++ dash-board/app/api/auth/[...nextauth]/route.ts
@@ -2,17 +2,15 @@ import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
 // import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({
+export const authOptions = {
   providers: [
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_CLIENT_ID!,
-    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    // }),
     Github({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     }),
   ],
-});
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
