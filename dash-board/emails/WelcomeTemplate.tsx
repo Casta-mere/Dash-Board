f4cca13a4a232ecd107853ca2c5df1e7995e1811
Casta-mere <castamerego@gmail.com>
2024-02-28 10:07:46 +0800

    Styling Email

--- dash-board/emails/WelcomeTemplate.tsx
+++ dash-board/emails/WelcomeTemplate.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { CSSProperties } from "react";
 import {
   Html,
   Body,
   Container,
+  Tailwind,
   Text,
   Link,
   Preview,
@@ -12,15 +13,18 @@ const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
       <Preview>Welcome aborad!</Preview>
-      <Body>
-        <Container>
-          <Text>Hello {name}</Text>
-          <Link href="http://dino.castamerego.com">
-            www.dino.castamerego.com
-          </Link>
-        </Container>
-      </Body>
+      <Tailwind>
+        <Body className="bg-white">
+          <Container>
+            <Text className="font-bold text-3xl">Hello {name}</Text>
+            <Link href="http://dino.castamerego.com">
+              www.dino.castamerego.com
+            </Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
 };
+
 export default WelcomeTemplate;
