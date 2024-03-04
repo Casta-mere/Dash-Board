2bf9d559f1f003f17bf33cd473eb4a97d5723f22
Casta-mere <castamerego@gmail.com>
2024-03-04 09:35:43 +0800

    Optimize Image

--- dash-board/app/page.tsx
+++ dash-board/app/page.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import ProductCard from "./components/ProductCard/ProductCard";
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import Image from "next/image";
+import RDR2 from "@/public/RDR2.png";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
@@ -11,6 +13,7 @@ export default async function Home() {
         <h1>Hello {session && session.user!.name}!</h1>
         <Link href="/users">Users</Link>
         <ProductCard />
+        {/* <Image src={RDR2} alt="An image of RDR2" /> */}
       </main>
     </>
   );
